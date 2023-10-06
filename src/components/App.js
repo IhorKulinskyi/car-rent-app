0b34fcb4ae150206fb84877a7b8b26b7bbf08343
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-10-06 14:54:43 +0300

    basic routing added

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import Layout from "./Layout/Layout";
+
+const HomePage = lazy(() => import("../pages/HomePage"));
+const Catalog = lazy(() => import("../pages/Catalog"));
+const Favorites = lazy(() => import("../pages/Favorites"));
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route index element={<HomePage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+export default App;
